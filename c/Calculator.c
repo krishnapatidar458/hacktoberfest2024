4d4313ee0ce176611caa6bf05bd813832a2124eb
Om Bhagwat <ombhagwat240103@gmail.com>
2023-09-30 23:57:24 +0530

    Calculator Program Added in file Calculator.c

--- c/Calculator.c
+++ c/Calculator.c
@@ -1,1 +1,41 @@
 
+#include <stdio.h>
+
+int main()
+{
+    double num1, num2;
+    char opr;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &opr);
+    printf("\n");
+
+    switch (opr)
+    {
+    case '+':
+        printf("%.2lf + %.2lf = %.2lf", num1, num2, num1 + num2);
+        break;
+    case '-':
+        printf("%.2lf - %.2lf = %.2lf", num1, num2, num1 - num2);
+        break;
+    case '*':
+        printf("%.2lf * %.2lf = %.2lf", num1, num2, num1 * num2);
+        break;
+    case '/':
+        if (num2 != 0)
+            printf("%.2lf / %.2lf = %.2lf", num1, num2, num1 / num2);
+        else
+            printf("Error: Division by zero!");
+        break;
+    default:
+        printf("Error: Invalid operator!");
+    }
+
+    return 0;
+}
