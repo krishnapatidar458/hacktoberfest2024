0930fe3962457afb37e4d1e08d4729d6fb53beb9
Om Bhagwat <ombhagwat240103@gmail.com>
2023-10-01 10:21:18 +0530

    Update code, Added funcationality to not to ternimate calculator upto user wants

--- c/Calculator.c
+++ c/Calculator.c
@@ -1,40 +1,50 @@
-
 #include <stdio.h>
 
 int main()
 {
     double num1, num2;
     char opr;
+    char choice;
 
-    printf("Enter first number: ");
-    scanf("%lf", &num1);
-
-    printf("Enter second number: ");
-    scanf("%lf", &num2);
-
-    printf("Enter an operator (+, -, *, /): ");
-    scanf(" %c", &opr);
-    printf("\n");
-
-    switch (opr)
+    while (true)
     {
-    case '+':
-        printf("%.2lf + %.2lf = %.2lf", num1, num2, num1 + num2);
-        break;
-    case '-':
-        printf("%.2lf - %.2lf = %.2lf", num1, num2, num1 - num2);
-        break;
-    case '*':
-        printf("%.2lf * %.2lf = %.2lf", num1, num2, num1 * num2);
-        break;
-    case '/':
-        if (num2 != 0)
-            printf("%.2lf / %.2lf = %.2lf", num1, num2, num1 / num2);
-        else
-            printf("Error: Division by zero!");
-        break;
-    default:
-        printf("Error: Invalid operator!");
+        printf("Enter first number: ");
+        scanf("%lf", &num1);
+
+        printf("Enter second number: ");
+        scanf("%lf", &num2);
+
+        printf("Enter an operator (+, -, *, /): ");
+        scanf(" %c", &opr);
+        printf("\n");
+
+        switch (opr)
+        {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("%.2lf / %.2lf = %.2lf", num1, num2, num1 / num2);
+            else
+                printf("Error: Division by zero!");
+            break;
+        default:
+            printf("Error: Invalid operator!");
+        }
+
+        printf("\n\nEnter 'c' to continue, Enter 'q' to exit\n");
+        scanf(" %c", &choice);
+        if (choice == 'q')
+        {
+            break;
+        }
     }
 
     return 0;
