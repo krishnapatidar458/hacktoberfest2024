e4808194599caaa55be4a99f07a0a8ccaaf4474e
bhoomikarao22 <95897461+bhoomikarao22@users.noreply.github.com>
2023-10-02 14:20:00 +0530

    Update Check.py
    This code is an update of the previous version that takes user input now and then checks if the number is smaller

--- python/Check.py
+++ python/Check.py
@@ -1,26 +1,17 @@
-
-
 def check(list1, val):
+    for x in list1:
+        if val >= x:
+            return False
+    return True
 	
-	
-	for x in list1:
+# Taking list input from user
+input_list = input("Enter a list of numbers separated by spaces: ")
+list1 = [int(item) for item in input_list.split()]
 
-		
-		if val>= x:
-			return False
-	return True
-	
-	
+# Taking input from user for 'val'
+val = int(input("Enter a value: "))
 
-list1 =[10, 20, 30, 40, 50, 60]
-val = 5
-if(check(list1, val)):
-	print("Yes")
+if check(list1, val):
+    print("Yes")
 else:
-	print("No")
-
-val = 20
-if(check(list1, val)):
-	print("Yes")
-else:
-	print("No")
+    print("No")
